c80c44ccfaa623456d910e9506fc42062c794c99
606-15\student <FCA2306100006@edu.fca.ac.jp>
2024-09-12 17:18:23 +0900

    0905日変更分

---
+++ Assets/script/igaguriController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class igaguriController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic=true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0,200,2000));
+    }
+}
